7f43565db34f23592ce552eea92aa7fcfcb17633
Monika 1588 <vermamonika3733@gmail.com>
2026-02-01 18:13:51 +0530

    commit

--- src/data/cardsData.jsx
+++ src/data/cardsData.jsx
@@ -1,14 +1,14 @@
 const cardsData = [
     { name: "star", image: "/images/starr.jpeg" },
     { name: "moon", image: "/images/moon.avif" },
-    { name: "heart", image: "/images/heart.jpg" },
+    { name: "heart", image: "/images/panda.png" },
     { name: "sun", image: "/images/sun.webp" },
     { name: "magic", image: "/images/cartoon.jpg" },
     { name: "crystal", image: "/images/laptop.jpeg" },
     { name: "flower", image: "/images/flower.jpg" },
     { name: "tree", image: "/images/icecream.jpeg" },
     { name: "cat", image: "/images/cat.png" },
-    { name: "dog", image: "/images/dog.avif" }
+    { name: "dog", image: "/images/dog.jpg" }
   ];
   
   export default cardsData;
